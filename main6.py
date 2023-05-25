13d55f915c5065005e6dd2e433878ea61ad262cb
Pavel <pmaskarev@vk.com>
2023-05-25 10:44:05 +0300

    Modified Task6(Added the mechanism of finding the lucky ticket)

--- main6.py
+++ main6.py
@@ -2,4 +2,21 @@ TicketNumber = input('Введите номер билета:')
 if len(TicketNumber)<6 or len(TicketNumber)>6:
     print('Билет не шестизначный.')
 else:
-   print('Good')
+   i = 0
+   NumberSumm1 = 0
+   while i<=2:
+       NumberSumm1 += int(TicketNumber[i])
+       i=i+1
+   print(f'Сумма первых трёх цифр - {NumberSumm1}')
+   i = 3
+   NumberSumm2 = 0
+   while i<=len(TicketNumber)-1:
+       NumberSumm2 += int(TicketNumber[i])
+       i=i+1
+   print(f'Сумма последних трёх цифр - {NumberSumm2}')
+   if NumberSumm1 == NumberSumm2:
+       print(f'{NumberSumm1}={NumberSumm2}')
+       print('Ваш билет счастливый!')
+   else:
+       print(f'{NumberSumm1}!={NumberSumm2}')
+       print('Ваш билет не оказался счастливым:(')
